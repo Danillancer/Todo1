c75f93df15feddd652ead341f40a2afa86a406bf
Данил Голубов <76730781+Danillancer@users.noreply.github.com>
2021-09-09 17:44:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import { useState } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+
+function App() {
+const [todos, setToDo]= useState([])
+
+
+const addTask = (userInput)=>{
+if(userInput){
+const newItem={
+  id:Math.random().toString(36).substr(2,9),
+  task: userInput,
+  complete: false,
+}
+setToDo([...todos, newItem])
+
+}
+}
+const removeTask = (id)=>{
+  setToDo([...todos.filter((todo) => todo.id !== id)])
+}
+
+const handleToggle = (id)=>{
+setToDo([...todos.map((todo)=> 
+  todo.id === id ? {...todo, complete:!todo.complete} : {...todo}
+)])
+} 
+
+  return (
+    <div className="App">
+   <header>
+     <h1>Cписок задач: {todos.length}</h1>
+    </header>
+
+
+    <TodoForm addTask={addTask}/>
+
+    {todos.map((todo)=>{
+      return(
+      <Todo
+      todo={todo}
+      key={todo.id}
+      toggleTask={handleToggle}
+      removeTask={removeTask}
+    />
+  )
+})}
+
+    </div>
+  );
+}
+
+export default App;
